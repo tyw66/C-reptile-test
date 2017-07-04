8dbe9c2d08667136214ca3d17dec79a803f0c2de
tyw66 <2639144635@qq.com>
2017-07-04 15:44:50 +0800

    稍完善了下线程部分

--- 线程/Form1.cs
+++ 线程/Form1.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace 线程Thread
@@ -21,43 +14,33 @@ namespace 线程Thread
         private void Form1_Load(object sender, EventArgs e)
         {
             string strInfo = string.Empty;
-            Thread t1 = new Thread(new ThreadStart(ThreadFunction));
-  t1.Start();
-            Thread t2 = new Thread(new ThreadStart(ThreadFunction1));
-            t2.Priority = ThreadPriority.Highest;
+            Thread t1 = new Thread(new ThreadStart(ThreadFuncA));            
+            Thread t2 = new Thread(new ThreadStart(ThreadFuncB));
 
-          
+            t1.Start();
+            t2.Priority = ThreadPriority.Highest;
             t2.Start();
             
-            
-
             //strInfo = "新建子线程唯一标识符：" + t1.ManagedThreadId;
             //strInfo += "\n名称：" + t1.Name;
             //strInfo += "\n状态：" + t1.ThreadState;
             //strInfo += "\n优先级：" + t1.Priority;
             //strInfo += "\n是否后台：" + t1.IsBackground;
-
             //richTextBox1.Text = strInfo;
 
             Thread.Sleep(500);//主线程休眠
-
-
-
             t1.Abort();
             t2.Abort();
-           // t.Join();
-           // MessageBox.Show("新建子线程结束", "新建子线程");
-
-
-
-
+            // t.Join();
+            // MessageBox.Show("新建子线程结束", "新建子线程");
         }
 
-        private void ThreadFunction()
+
+        private void ThreadFuncA()
         {
             MessageBox.Show("新建子线程1开始执行", "新建子线程1");
         }
-        private void ThreadFunction1()
+        private void ThreadFuncB()
         {
             MessageBox.Show("新建子线程2开始执行", "新建子线程2");
         }
--- 线程同步/Program.cs
+++ 线程同步/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace 线程同步
 {
@@ -15,31 +11,84 @@ namespace 线程同步
             for (int ii = 0; ii < 3; ii++)
             {
                 Thread th = new Thread(t.TestRun);
-
+                Console.WriteLine(">>第{0}个进程开始", ii + 1);
                 th.Start();
-                Console.WriteLine(">>{0}个进程开始", ii+1);
             }
 
+            //TestJoin.test_join();
 
             Console.Read();
-
         }
+
 
         class TestLock
         {
             private Object obj = new object();
-            private int i = 0;
+            private int testNumber = 0;
             public void TestRun()
             {
+                //上锁，保证锁里面的代码/变量资源在1个时刻只有1个进程访问
                 lock (obj)
                 {
-                    Console.WriteLine("i的初始值为：" + i.ToString());
-                   Thread.Sleep(1);
-                    i++;
-                    Console.WriteLine("i在自增后的值为：" + i.ToString());
+                    Console.WriteLine("i的初始值为：" + testNumber.ToString());
+                    Thread.Sleep(2000);
+                    testNumber++;
+                    Console.WriteLine("i在自增后的值为：" + testNumber.ToString());
                 }
             }
-
         }
+
+
+        class TestJoin
+        {
+            //一个例子说明join()的作用
+            public static void test_join()
+            {
+                //线程A
+                Thread ThreadA = new Thread(delegate ()
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.WriteLine("A : ");
+                        if (i == 9)
+                        {
+                            break;
+                        }
+                        Thread.Sleep(1000);
+                    }
+                });
+                //线程B
+                Thread ThreadB = new Thread(delegate ()
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine("B : ");
+                        if (i == 4)
+                        {
+                            break;
+                        }
+                        Thread.Sleep(1000);
+                    }
+                    //在这里插入线程A
+                    //线程B中join了线程A只有A执行结束，才可以继续执行线程B中剩下的代码
+                    ThreadA.Join();
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine("C : ");
+                        if (i == 4)
+                        {
+                            break;
+                        }
+                        Thread.Sleep(1000);
+                    }
+                });
+                ThreadA.Start();
+                ThreadB.Start();
+                Console.ReadLine();
+            }
+        }
+        
+
+
     }
 }
